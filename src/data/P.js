0934767ca7e6cb819df1d297f7d77b37dc8c6c86
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-11-05 11:17:01 +0100

    work on P stops

---
+++ src/data/P.js
@@ -0,0 +1,19 @@
+import { remainingStops as stopsUnsorted } from "./OSL";
+
+const stations = stopsUnsorted.filter(
+  stop =>
+    stop.name.includes("S Potsdam Hauptbahnhof") ||
+    stop.name.includes("S Babelsberg") ||
+    stop.name.includes("S Griebnitzsee")
+);
+const remainingAfterStations = stopsUnsorted.filter(
+  stop => stations.indexOf(stop) === -1
+);
+const stops = remainingAfterStations.filter(stop =>
+  stop.name.startsWith("Potsdam,")
+);
+const remainingStops = remainingAfterStations.filter(
+  stop => stops.indexOf(stop) === -1
+);
+
+export { stations, stops, remainingStops };
