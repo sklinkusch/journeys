ebc6ce985ad336b9c3162b5068d0b300716468bd
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-11-05 10:57:47 +0100

    work on CB stops

---
+++ src/data/CB.js
@@ -0,0 +1,14 @@
+import { remainingStops as stopsUnsorted } from "./BRB";
+
+const stops = stopsUnsorted.filter(
+  stop =>
+    stop.name.startsWith("Cottbus,") ||
+    stop.name.startsWith("Haasow,") ||
+    stop.name.startsWith("Hänchen,") ||
+    stop.name.startsWith("Klein Gaglow,") ||
+    stop.name.startsWith("Kolkwitz,")
+);
+
+const remainingStops = stopsUnsorted.filter(stop => stops.indexOf(stop) === -1);
+
+export { stops, remainingStops };
