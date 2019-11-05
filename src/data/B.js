f778ee7f2d14e7c94ced326a64c9ca8c36a069d4
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2019-11-05 10:44:03 +0100

    work on stops in Berlin

---
+++ src/data/B.js
@@ -0,0 +1,31 @@
+// import unsorted stop array
+import stopsUnsorted from "./unsorted";
+// get all stops from Berlin
+const stopsFilter = stopsUnsorted.filter(
+  stop =>
+    stop.name.includes("(Berlin)") ||
+    stop.name.includes("(Bln)") ||
+    stop.name.startsWith("Berlin Hauptbahnhof") ||
+    stop.name === "U Stadtmitte/Krausenstr." ||
+    stop.name === "U Alexanderplatz [Bus]" ||
+    stop.name === "S Rahnsdorf [Tram]"
+);
+const stops = stopsFilter.map(stop => {
+  if (stop.name.startsWith("Berlin,")) {
+    return { id: stop.id, name: stop.name.substr(8) };
+  } else if (stop.name.endsWith("(Berlin)")) {
+    return { id: stop.id, name: stop.name.substring(0, stop.name.length - 9) };
+  } else if (stop.name.includes("(Bln)")) {
+    const newName = stop.name.replace("(Bln)", "");
+    return { id: stop.id, name: newName };
+  } else if (stop.name.includes("(Berlin)")) {
+    const newName = stop.name.replace("(Berlin)", "");
+    return { id: stop.id, name: newName };
+  }
+  return { id: stop.id, name: stop.name };
+});
+
+export const remainingStops = stopsUnsorted.filter(
+  stop => stopsFilter.indexOf(stop) === -1
+);
+export default stops;
